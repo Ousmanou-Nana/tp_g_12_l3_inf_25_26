4626819667374c0d0acda57585197dd4b2517402
oth682 <osmanounana@gmail.com>
2025-12-17 05:49:39 +0100

    add screen AddObject

--- app/src/main/java/com/example/tp_g_12_l3_inf_25_26/ui/admin/home/HomeAdminFragment.java
+++ app/src/main/java/com/example/tp_g_12_l3_inf_25_26/ui/admin/home/HomeAdminFragment.java
@@ -11,8 +11,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.tp_g_12_l3_inf_25_26.R;
+import com.example.tp_g_12_l3_inf_25_26.ui.admin.addobject.AddObjectFragment;
 
 public class HomeAdminFragment extends Fragment {
 
@@ -28,11 +30,33 @@ public class HomeAdminFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home_admin, container, false);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(HomeAdminViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
+        Button buttonAddObject = view.findViewById(R.id.buttonAddObject);
+        buttonAddObject.setOnClickListener(v -> {
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.main_admin_container, AddObjectFragment.newInstance())
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+//        Button buttonListDeclarations = view.findViewById(R.id.buttonListDeclarations);
+//        buttonListDeclarations.setOnClickListener(v -> {
+//            getParentFragmentManager().beginTransaction()
+//                    .replace(R.id.main_admin_container, ListDeclarationFragment.newInstance())
+//                    .addToBackStack(null)
+//                    .commit();
+//        });
+//
+//        Button buttonListObject = view.findViewById(R.id.buttonlistObject);
+//        buttonListObject.setOnClickListener(v -> {
+//            getParentFragmentManager().beginTransaction()
+//                    .replace(R.id.main_admin_container, ListObjectFragment.newInstance())
+//                    .addToBackStack(null)
+//                    .commit();
+//        });
+    }
 }
+
